3654d7abdf4383b61faeaa5569316c4d7ad1d465
Chernikov Nikita <14nik20@gmail.com>
2025-01-02 07:04:27 +0200

    Added ValidationBehavior for FluentValidation

---
+++ DotNetCommon/Application/Services/FluentValidationService.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Services;
+public static class FluentValidationService
+{
+    public static string FluentValidationFailuresToString(IEnumerable<ValidationFailure> failures)
+    {
+        return string.Join(Environment.NewLine, failures.Select(f => f.ErrorMessage));
+    }
+}
